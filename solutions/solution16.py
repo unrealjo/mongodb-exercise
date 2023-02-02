ab5ad6ef9cfb30b5080690b28ce2be5be9aaa07a
Youssef Eddaif <jo9unreal@gmail.com>
2023-02-02 22:01:19 +0100

    adding solutions 16 and 17

---
+++ solutions/solution16.py
@@ -0,0 +1,20 @@
+from config.client import Client
+db = Client().get_db("practice")
+
+options = {
+    "restaurant_id":1,
+    "name":1,
+    "borough":1,
+    "cuisine":1
+}
+
+documenets  = db.restaurants.find(
+    {"borough":
+        {"$in":["Staten Island","Queens","Bronx","Brooklyn"]}
+    },
+    options
+)
+
+for doc in documenets :
+    print(doc)
+
